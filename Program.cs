fdd5737e019d847d734d0f984f48e3f5e928a568
helder <jose.silva2000@etec.sp.gov.br>
2022-09-17 09:51:20 -0300

    resolucção do leet

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("digite seu nome completo ");
+string nomeDigitado = Console.ReadLine()!;
+string frase = Console.ReadLine()!;
+string codificador = frase
+.Replace("A", "4")
+.Replace("a", "4")
+.Replace("e", "3")
+.Replace("E", "3")
+.Replace("i", "1")
+.Replace("I", "1")
+.Replace("L", "1")
+.Replace("O", "0")
+.Replace("T", "7")
+.Replace("S", "5")
+.Replace("s", "5");
+Console.Write(codificador);
